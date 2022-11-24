960621393f263320f2be2fa6074f610ca23df446
Slmmaula <117788112+Slmmaula@users.noreply.github.com>
2022-11-24 23:00:58 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["PERSIB","PERSIBapp","Liga1"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        cursorChar: ' |',
+        smartBackspace: true,
+        loop: true
+    });
